3332a3117154d3526743a61fb6b48681d0feebde
이승렬 <ibban9810@icloud.com>
2023-11-07 18:02:24 +0900

    Feat: 2일차 문제 업로드

---
+++ src/saffy/D2/q1204/Solution.java
@@ -0,0 +1,34 @@
+package saffy.D2.q1204;
+
+import java.util.Scanner;
+
+class Solution {
+
+    public static int findMax(int[] arr) {
+        int MAX = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (MAX <= arr[i]) {
+                MAX = arr[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int T;
+
+        T = scanner.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int i = scanner.nextInt();
+            int[] arr = new int[101];
+            for (int j = 0; j < 1000; j++) {
+                int tmp = scanner.nextInt();
+                arr[tmp]++;
+            }
+            System.out.println("#" + i + " " + findMax(arr));
+        }
+    }
+}
