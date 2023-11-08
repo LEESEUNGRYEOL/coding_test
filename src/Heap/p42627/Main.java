cfa118645bc12ca809e7cc4c969f827c29d8cfb8
이승렬 <ibban9810@icloud.com>
2023-11-08 15:41:10 +0900

    Feat: 3일차 문제 업로드

---
+++ src/Heap/p42627/Main.java
@@ -0,0 +1,35 @@
+package Heap.p42627;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int[] scoville, int k) {
+        int result = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int a : scoville) {
+            priorityQueue.add(a);
+        }
+        while (priorityQueue.peek() < k) {
+            if (priorityQueue.size() == 1) {
+                return -1;
+            }
+            int a = priorityQueue.poll();
+            int b = priorityQueue.poll();
+            priorityQueue.add(a + (b * 2));
+            result++;
+        }
+        return result;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] scoville = {12, 2, 3, 9, 10, 1};
+        int k = 7;
+        Solution solution = new Solution();
+        System.out.println(solution.solution(scoville, k));
+    }
+}
