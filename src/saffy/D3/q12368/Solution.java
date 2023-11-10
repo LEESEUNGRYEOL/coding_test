b6840f59263372b2cf4bf240dcbf85cb7a4fde29
이승렬 <ibban9810@icloud.com>
2023-11-10 15:34:46 +0900

    Feat: 5일차 문제 업로드

---
+++ src/saffy/D3/q12368/Solution.java
@@ -0,0 +1,21 @@
+package saffy.D3.q12368;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int T;
+
+        T = scanner.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int result = a + b;
+            while (result - 24 >= 0) {
+                result -= 24;
+            }
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
+}
